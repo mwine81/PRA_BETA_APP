18a94ce68eaff93e4e60e2fee5954584d4f24ff3
mwine <mwine81@gmail.com>
2025-05-30 12:29:13 -0400

    Fix casing for hospital340B file path in configuration

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-    
+
 
 BASE_DIR =Path('DATABASE')
 PAYMENT_INFO = BASE_DIR / "db.parquet"
@@ -7,7 +7,7 @@ NDC_NAMES = BASE_DIR / "ndc_names.parquet"
 HCPCS_DESC = BASE_DIR / "hcpcs_desc.parquet"
 HOSPITALS = BASE_DIR / "hospital.parquet"
 PRICE_PATH = BASE_DIR / 'prices.parquet'
-HOSPITAL340B = BASE_DIR / 'hospital340b.parquet'
+HOSPITAL340B = BASE_DIR / 'hospital340B.parquet'
 
 
 
