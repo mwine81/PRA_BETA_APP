3c8498372c7c28867a26912f42d19269fea34661
mwine <mwine81@gmail.com>
2025-06-19 20:28:42 -0400

    Revert "Enhance UI components with expanded modals for map and distribution plots; add icons to control buttons and improve layout organization."
    This reverts commit 2dafa6bfbc9d969f76cc40c0b01da739aac820f3.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import shutil
+
 
 BASE_DIR =Path('DATABASE')
 PAYMENT_INFO = BASE_DIR / "db.parquet"
@@ -10,15 +10,5 @@ PRICE_PATH = BASE_DIR / 'prices.parquet'
 HOSPITAL340B = BASE_DIR / 'hospital340B.parquet'
 
 
-def update_files():
-    FROM_DIR = Path(r'C:\Users\mwine\Projects\MAY_2025\dev\HOSPITAL_APP')
-    TO = Path().cwd()
-    shutil.copy(FROM_DIR / 'app4.py', TO / 'app.py')
-    shutil.copy(FROM_DIR / 'assets/custom.css', TO / 'assets/custom.css')
-    shutil.copy(FROM_DIR / 'ag_grid_def.py', TO / 'ag_grid_def.py')
-    shutil.copy(FROM_DIR / 'data_dictionary_table_schema.py', TO / 'data_dictionary_table_schema.py')
-    shutil.copy(FROM_DIR / 'helpers.py', TO / 'helpers.py')
 
-if __name__ == "__main__":
-    update_files()
-    print("Files updated successfully.")
+    
